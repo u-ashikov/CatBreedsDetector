79687e07320f8f0c9600ec184d17b578fee11264
Yulian Ashikov <u.ashikov@abv.bg>
2020-05-07 20:12:55 +0300

    Added console application for testing.

---
+++ CatBreedsDetector/TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using CatBreedsDetector.Classification;
+using Microsoft.ML;
+
+namespace TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var classifier = new CatBreedClassifier();
+
+            ITransformer model = classifier.GenerateModel();
+
+            classifier.ClassifySingleImage(model);
+        }
+    }
+}
