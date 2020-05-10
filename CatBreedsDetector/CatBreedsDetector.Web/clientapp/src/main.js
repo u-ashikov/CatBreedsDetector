5c88e7cb118f4d7e6ceefe847b9b8ed84a46a192
Yulian Ashikov <u.ashikov@abv.bg>
2020-05-11 00:07:12 +0300

    Added bootstrap to the client app.

--- CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/main.js
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
