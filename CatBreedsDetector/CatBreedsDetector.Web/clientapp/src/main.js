06bdc2a4b0bcc2180e4be1ea2e574fb3b429eebd
Yulian Ashikov <u.ashikov@abv.bg>
2020-05-12 23:33:37 +0300

    Added vue router. Created routes for home and about.

--- CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/main.js
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { router } from './routing/router'
 
 import 'jquery/src/jquery.js'
 import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    render: h => h(App)
+});
---
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/routing/router.js
@@ -0,0 +1,14 @@
+﻿import VueRouter from 'vue-router'
+
+import Home from '../components/Home'
+import About from '../components/About'
+
+const router = new VueRouter({
+    routes: [
+        { path: '', component: Home },
+        { path: '/about', component: About }
+    ],
+    mode: "history"
+});
+
+export { router }
