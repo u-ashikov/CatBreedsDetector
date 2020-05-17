07434277d0ac15d4ed1fe04d75a2e495b55bc1c2
Yulian Ashikov <u.ashikov@abv.bg>
2020-05-17 23:18:23 +0300

    Installed axios. Created service for calling the API for identifying the cat breed.

---
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/service/file-service.js
@@ -0,0 +1,13 @@
+﻿import axios from 'axios';
+
+var axiosInstance = axios.create({
+    baseURL: 'http://localhost:50598/api'
+});
+
+function upload(formData) {
+    return axiosInstance.post('/CatBreeds/DetectAsync', formData);
+}
+
+export default {
+    upload: upload
+};
