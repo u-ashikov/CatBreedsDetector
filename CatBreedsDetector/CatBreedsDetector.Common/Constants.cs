7b373f84a75424d0d957fae745dd718fe477c41e
Yulian Ashikov <u.ashikov@abv.bg>
2020-05-20 23:45:06 +0300

    Created class library for common stuff. Added constants.

--- CatBreedsDetector/CatBreedsDetector.Common/Constants.cs
+++ CatBreedsDetector/CatBreedsDetector.Common/Constants.cs
@@ -1,10 +1,12 @@
-﻿namespace CatBreedsDetector.Web.Common
+﻿namespace CatBreedsDetector.Common
 {
     public class Constants
     {
         public class Message
         {
             public const string ProvideFileImage = "You must provide a file!";
+
+            public const string MissingOrInvalidFile = "File is missing or invalid!";
         }
 
         public class FilePath
