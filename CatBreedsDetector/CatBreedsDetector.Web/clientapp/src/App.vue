acfcc868b2225661c273743ee7f5a1e2b8c6f242
Yulian Ashikov <u.ashikov@abv.bg>
2020-05-21 19:24:57 +0300

    Added footer component.

--- CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/App.vue
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/App.vue
@@ -3,17 +3,20 @@
       <img class="img-thumbnail rounded-circle" src="../public/cat-paw-logo.png"/>
       <h1 class="display-3 mt-3">Cat Breeds Detector</h1>
       <hr />
-      <NavBar message="Hello there"></NavBar>
+      <NavBar></NavBar>
+      <Footer></Footer>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-      NavBar
+      NavBar,
+      Footer
   }
 }
 </script>
---
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/components/Footer.vue
@@ -0,0 +1,16 @@
+﻿<template>
+    <footer id="footer" class="container text-center">
+        <hr />
+        Copyright &copy; Cat Breeds Detector, developed by YA Studios
+    </footer>
+</template>
+
+<style scoped>
+    #footer {
+        padding-top: 10px;
+        padding-bottom: 10px;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
