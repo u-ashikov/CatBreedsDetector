ca93fc70eedc8734d99b3f5546e5f8af9871f417
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-01 22:38:51 +0300

    Added training ground for training ML model. Created new tags.tsv file with over 1400 cats images.

---
+++ CatBreedsDetector/CatBreedsDetector.TrainingGround/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CatBreedsDetector.TrainingGround
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            if (!Directory.Exists("cats"))
+            {
+                return;
+            }
+
+            var directories = Directory.GetDirectories("cats");
+
+            using (var fileWriter = new StreamWriter("tags.tsv", true))
+            {
+                foreach (var directory in directories)
+                {
+                    var directoryInfo = new DirectoryInfo(directory);
+                    var files = Directory.GetFiles(directory);
+
+                    foreach (var file in files)
+                    {
+                        var fileInfo = new FileInfo(file);
+                        
+                        fileWriter.WriteLine(string.Concat(fileInfo.Name, "\t", directoryInfo.Name));
+                    }
+                }
+            }
+        }
+    }
+}
