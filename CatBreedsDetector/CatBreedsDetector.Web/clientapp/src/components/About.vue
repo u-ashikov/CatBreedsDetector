187a1521c8d92023bfbab1292f72d4c4403c7162
Yulian Ashikov <u.ashikov@abv.bg>
2020-06-07 00:06:05 +0300

    Added more info in the about page.

--- CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/components/About.vue
+++ CatBreedsDetector/CatBreedsDetector.Web/clientapp/src/components/About.vue
@@ -5,5 +5,11 @@
         <p class="text-left">
             Cat Breeds Detector is a simple application built just for fun, which detects (currently) the most common cat breeds based on the uploaded picture.
         </p>
+        <p class="text-left">
+            The Cat Breeds Detector uses pre-trained machine learning model for predicting the cat breed. The model is trained on over 2,000 cat images and can recognize: Abyssinian, Bengal, Birman, Bombay, British Shorthair, Egyptian Mau, Maine Coon, Persian, Ragdoll, Russian blue, Siamese and Sphynx cats.
+        </p>
+        <p class="text-left">
+            For more information about the cat breeds, you can visit the following link: <a href="https://www.petfinder.com/cat-breeds/" target="_blank">Cat Breeds</a>
+        </p>
     </div>
 </template>
