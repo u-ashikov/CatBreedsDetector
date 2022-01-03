17f8a3c718302222ba0df593fb541477f32a84a1
u-ashikov <u.ashikov@abv.bg>
2022-01-03 21:10:58 +0200

    Renamed the file

--- src/CatBreedsDetector.Client/src/services/CatBreedsService.ts
+++ src/CatBreedsDetector.Client/src/services/CatBreedsService.ts
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   baseURL: "http://localhost:59999/api",
 });
 
+// TODO: Should return a model containing information about the prediction or error.
 export function upload(formData: FormData) {
   return axiosInstance.post("/CatBreeds/DetectAsync", formData);
 }
