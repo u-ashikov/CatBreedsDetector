79b007355037c7b81bc405f369bd7c5891a3bd0e
u-ashikov <u.ashikov@abv.bg>
2022-01-16 12:50:59 +0200

    Move components

--- src/CatBreedsDetector.Client/src/components/common/About.tsx
+++ src/CatBreedsDetector.Client/src/components/common/About.tsx
@@ -4,8 +4,6 @@ export default class About extends React.Component {
   public render(): JSX.Element {
     return (
       <div>
-        <h1 className="text-left">About</h1>
-        <hr />
         <p className="text-left">
           Cat Breeds Detector is a simple application built just for fun, which
           detects (currently) the most common cat breeds based on the uploaded
