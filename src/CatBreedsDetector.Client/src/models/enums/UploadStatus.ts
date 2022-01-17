576099883a2f92aba2128fe2d86bd793e8a595f1
u-ashikov <u.ashikov@abv.bg>
2022-01-17 21:14:47 +0200

    Added detection result model and upload status model

---
+++ src/CatBreedsDetector.Client/src/models/enums/UploadStatus.ts
@@ -0,0 +1,6 @@
+export enum UploadStatus {
+  Initial = 0,
+  Saving = 1,
+  Success = 2,
+  Failed = 3,
+}
--- src/CatBreedsDetector.Client/src/services/CatBreedsService.ts
+++ src/CatBreedsDetector.Client/src/services/CatBreedsService.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 import { getApiUrl } from "../helpers/apiHelpers";
 
 // TODO: Should return a model containing information about the prediction or error.
-export function upload(formData: FormData) {
+export function uploadImage(imageToUpload: File) {
   const apiUrl = getApiUrl();
+
+  const formData = new FormData();
+  formData.append("catImage", imageToUpload);
 
   return axios.post(`${apiUrl}/api/CatBreeds/DetectAsync`, formData);
 }
