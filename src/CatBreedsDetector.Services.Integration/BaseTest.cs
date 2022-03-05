39161b743d3f125c55d8f47af5a2e97b907c66ec
u-ashikov <u.ashikov@abv.bg>
2022-03-06 01:07:04 +0200

    Written service integration tests for deleting a file in the FileService

---
+++ src/CatBreedsDetector.Services.Integration/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿namespace CatBreedsDetector.Services.Integration
+{
+    using CatBreedsDetector.Services.Contracts;
+    using CatBreedsDetector.Services.Implementations;
+
+    public abstract class BaseTest
+    {
+        protected readonly IFileService _fileService;
+
+        public BaseTest()
+        {
+            this._fileService = new FileService();
+        }
+    }
+}
---
+++ src/CatBreedsDetector.Services.Integration/FileServiceIntegrationTests.cs
@@ -0,0 +1,77 @@
+﻿namespace CatBreedsDetector.Services.Integration
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Xunit;
+    using CatBreedsDetector.Tests.Common.Helpers;
+
+    public class FileServiceIntegrationTests : BaseTest
+    {
+        [Theory]
+        [MemberData(nameof(GetRandomFileNames))]
+        public void DeleteFilesInDirectoryShouldDeleteNothingWithNonExistingDirectory(string randomFileName)
+        {
+            // Arrange
+            var filePath = "c:\\Temp\\TestFiles";
+            var fileName = $"{filePath}\\{TestsHelpers.GenerateRandomString()}";
+            this.CreateFileInDirectory(filePath, fileName);
+
+            // Act
+            this._fileService.DeleteFilesInDirectory(randomFileName);
+
+            // Assert
+            var existingFiles = Directory.GetFiles(filePath);
+            Assert.NotEmpty(existingFiles);
+
+            // Clean up
+            File.Delete(fileName);
+            Directory.Delete(filePath);
+            Assert.False(Directory.Exists(filePath));
+        }
+
+        [Fact]
+        public void DeleteFilesInDirectoryShouldDeleteTheFilesSuccessfully()
+        {
+            // Arrange
+            var filePath = "c:\\Temp\\TestFiles";
+            var fileName = $"{filePath}\\{TestsHelpers.GenerateRandomString()}";
+            this.CreateFileInDirectory(filePath, fileName);
+
+            // Act
+            this._fileService.DeleteFilesInDirectory(filePath);
+
+            // Assert
+            var existingFiles = Directory.GetFiles(filePath);
+            Assert.Empty(existingFiles);
+
+            // Clean up
+            Directory.Delete(filePath);
+            Assert.False(Directory.Exists(filePath));
+        }
+
+        public static IEnumerable<object[]> GetRandomFileNames()
+        {
+            yield return new[] { (string)null };
+            yield return new[] { string.Empty };
+            yield return new[] { TestsHelpers.GenerateRandomString() };
+        }
+
+        private void CreateFileInDirectory(string filePath, string fileName)
+        {
+            if (Directory.Exists(filePath) == false)
+                Directory.CreateDirectory(filePath);
+
+            FileStream fileStream = null;
+
+            try
+            {
+                fileStream = File.Create(Path.Combine(fileName));
+            }
+            finally
+            {
+                if (fileStream != null)
+                    fileStream.Dispose();
+            }
+        }
+    }
+}
