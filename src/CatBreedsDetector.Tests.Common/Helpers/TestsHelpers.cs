23bfa1034cc4b1b66513b5706aabf774828929db
u-ashikov <u.ashikov@abv.bg>
2022-03-06 01:07:25 +0200

    Added helper method for generating random string

---
+++ src/CatBreedsDetector.Tests.Common/Helpers/TestsHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace CatBreedsDetector.Tests.Common.Helpers
+{
+    using System;
+    using System.Linq;
+
+    public static class TestsHelpers
+    {
+        public static string GenerateRandomString(int length = 20)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
