70be97bc56e368053bde5b12237e0c9513a5452b
u-ashikov <u.ashikov@abv.bg>
2023-03-04 01:22:52 +0200

    Using an assembly path for the predicted images

--- src/CatBreedsDetector.Server/Controllers/CatBreedsController.cs
+++ src/CatBreedsDetector.Server/Controllers/CatBreedsController.cs
@@ -1,12 +1,12 @@
-﻿using System.Threading;
-
-namespace CatBreedsDetector.Web.Controllers
+﻿namespace CatBreedsDetector.Web.Controllers
 {
     using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading;
     using System.Threading.Tasks;
     using CatBreedsDetector.Classification.Interfaces;
     using CatBreedsDetector.Common;
-    using CatBreedsDetector.Models;
     using CatBreedsDetector.Models.InputModels;
     using CatBreedsDetector.Models.ViewModels;
     using CatBreedsDetector.Services.Contracts;
@@ -16,8 +16,6 @@ namespace CatBreedsDetector.Web.Controllers
     [Route("api/[controller]")]
     public class CatBreedsController : ControllerBase
     {
-        private const string DirectoryPath = "C:\\Temp\\TestFiles";
-
         private readonly ICatBreedClassifier _catBreedClassifier;
 
         private readonly IFileService _fileService;
@@ -34,8 +32,9 @@ namespace CatBreedsDetector.Web.Controllers
             if (model is null || !this.ModelState.IsValid)
                 return this.BadRequest();
 
-            var predictedImagesDirectoryPath = DirectoryPath + $@"\{Constants.FilePath.PredictedImages}";
-            var imagePath = predictedImagesDirectoryPath + $@"\{model.CatImage.FileName}";
+            var rootPredictedImagesDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var predictedImagesDirectoryPath = Path.Combine(rootPredictedImagesDirectory, Constants.FilePath.PredictedImages);
+            var imagePath = Path.Combine(predictedImagesDirectoryPath, model.CatImage.FileName);
 
             this._fileService.DeleteFilesInDirectory(predictedImagesDirectoryPath);
 
