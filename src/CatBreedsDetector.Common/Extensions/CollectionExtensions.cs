fb2e3ac8afd72a1f89cc4456e828f7d9fa1bbeb5
u-ashikov <u.ashikov@abv.bg>
2023-03-07 18:40:56 +0200

    Added a new method to convert a single value into a collection

--- src/CatBreedsDetector.Common/Extensions/CollectionExtensions.cs
+++ src/CatBreedsDetector.Common/Extensions/CollectionExtensions.cs
@@ -1,5 +1,6 @@
 namespace CatBreedsDetector.Common.Extensions
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -16,5 +17,18 @@ namespace CatBreedsDetector.Common.Extensions
         /// <returns>A boolean indicating whether the collection is null or has no elements in it.</returns>
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
             => collection is null || !collection.Any();
+
+        /// <summary>
+        /// Use this method to create and array from a single element of a given type.
+        /// </summary>
+        /// <param name="input">The input element that should be converted to array.</param>
+        /// <typeparam name="T">The type of the input element that should be converted to array.</typeparam>
+        /// <returns>The produced array containing the input element.</returns>
+        public static T[] AsArray<T>(this T input)
+        {
+            ArgumentNullException.ThrowIfNull(input);
+
+            return new[] { input };
+        }
     }
 }
