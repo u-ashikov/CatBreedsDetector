3459fa694e8675e565234c53d8e452b798dedb0c
Yulian Ashikov <u.ashikov@abv.bg>
2023-04-11 23:25:07 +0300

    Added a controller and an endpoint to check whether the server is up and running

---
+++ src/CatBreedsDetector.Server/Common/ServerConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace CatBreedsDetector.Server.Common
+{
+    /// <summary>
+    /// A static class containing structured constants.
+    /// </summary>
+    public static class ServerConstants
+    {
+        /// <summary>
+        /// A static class containing common messages.
+        /// </summary>
+        public static class Messages
+        {
+            public const string ServerIsRunning = "The API Server is up and running.";
+        }
+    }
+}
---
+++ src/CatBreedsDetector.Server/Controllers/HeartBeatController.cs
@@ -0,0 +1,17 @@
+﻿namespace CatBreedsDetector.Server.Controllers
+{
+    using CatBreedsDetector.Server.Common;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HeartBeatController : ControllerBase
+    {
+        /// <summary>
+        /// Use this endpoint to check if the API server is up and running.
+        /// </summary>
+        /// <returns>An <see cref="IActionResult"/> containing information whether the API server is up and running.</returns>
+        [HttpGet("[action]")]
+        public IActionResult Get() => this.Ok(ServerConstants.Messages.ServerIsRunning);
+    }
+}
